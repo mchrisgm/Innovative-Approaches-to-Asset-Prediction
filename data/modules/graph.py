f5fd89fd156070fe5b993039aeff72ff15875223
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2024-07-14 04:18:45 +0100

    Changed graphing to plotly for speed

--- data/modules/graph.py
+++ data/modules/graph.py
@@ -1,22 +1,18 @@
-from io import BytesIO
-import matplotlib.axes
-import numpy as np
 import pandas as pd
-from PIL import Image
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from datetime import timedelta
+import numpy as np
+from PIL import Image, ImageFilter
+import plotly.graph_objects as go
+from io import BytesIO
+from plotly.subplots import make_subplots
 
 # Exported functions
 __all__ = ["create_image"]
 
-def create_candlestick(ax: matplotlib.axes.Axes, df):
+def create_candlestick(df):
     """
-    Creates a candlestick chart on the given axis for the DataFrame.
+    Creates a candlestick chart for the DataFrame.
 
     Parameters:
-    ax (matplotlib.axes.Axes): The axis on which to plot the candlestick chart.
     df (pd.DataFrame): DataFrame containing the stock data with columns Date, Open, High, Low, Close.
     """
     assert "Date" in df.columns, "Date column not found in the DataFrame"
@@ -24,36 +20,52 @@ def create_candlestick(ax: matplotlib.axes.Axes, df):
     assert "High" in df.columns, "High column not found in the DataFrame"
     assert "Low" in df.columns, "Low column not found in the DataFrame"
     assert "Close" in df.columns, "Close column not found in the DataFrame"
-    for idx, row in df.iterrows():
-        # high/low lines
-        ax.plot([row['Date'], row['Date']], 
-                [row['Low'], row['High']], 
-                color='#0000FF', linewidth=0.5)
-        # open marker
-        ax.plot([row['Date']-timedelta(hours=1), row['Date']-timedelta(hours=11)], 
-                [row['Open'], row['Open']], 
-                color='#0000FF', linewidth=1)
-        # close marker
-        ax.plot([row['Date']-timedelta(hours=1), row['Date']+timedelta(hours=11)], 
-                [row['Close'], row['Close']], 
-                color='#0000FF', linewidth=1)
+    
+    # f = go.Ohlc(
+    #         x=df['Date'],
+    #         open=df['Open'],
+    #         high=df['High'],
+    #         low=df['Low'],
+    #         close=df['Close']
+    #     )
+    # f.tickwidth = 0.5
+    # f.increasing.line.color = '#00FF00'
+    # f.decreasing.line.color = '#FF0000'
+    
+    f = go.Candlestick(
+            x=df['Date'],
+            open=df['Open'],
+            high=df['High'],
+            low=df['Low'],
+            close=df['Close'],
+            increasing_line_color='#00FF00',
+            decreasing_line_color='#FF0000',
+            line_width=1
+        )
+    f.whiskerwidth = 0
+    return f
 
-def create_line(ax, df):
+def create_line(df):
     """
-    Creates a line chart on the given axis for the DataFrame.
+    Creates a line chart for the DataFrame.
 
     Parameters:
-    ax (matplotlib.axes.Axes): The axis on which to plot the line chart.
     df (pd.DataFrame): DataFrame containing the bond index data with columns Date, Index.
     """
     assert "Date" in df.columns, "Date column not found in the DataFrame"
     assert "Index" in df.columns, "Index column not found in the DataFrame"
     
-    ax.plot(df['Date'], df['Index'], color='#0000FF', linewidth=1)
+    f = go.Scatter(
+            x=df['Date'],
+            y=df['Index'],
+            mode='lines',
+            line=dict(color='#0000FF', width=1),
+        )
+    return f
 
-def make_figure(equity_df, currency_df, bond_df, lookback=5):
+def make_figure(equity_df=None, currency_df=None, bond_df=None, lookback=5):
     """
-    Creates a matplotlib figure with subplots for equity, currency, and bond data.
+    Creates a Plotly figure with subplots for equity, currency, and bond data.
 
     Parameters:
     equity_df (pd.DataFrame): DataFrame containing the equity data.
@@ -61,33 +73,44 @@ def make_figure(equity_df, currency_df, bond_df, lookback=5):
     bond_df (pd.DataFrame): DataFrame containing the bond index data.
 
     Returns:
-    matplotlib.figure.Figure: Matplotlib Figure object.
+    plotly.graph_objects.Figure: Plotly Figure object.
     """
-    equity_df['Date'] = pd.to_datetime(equity_df['Date'], format='%d%b%Y:%H:%M:%S.%f')
-    currency_df['Date'] = pd.to_datetime(currency_df['Date'], format='%d%b%Y:%H:%M:%S.%f')
-    bond_df['Date'] = pd.to_datetime(bond_df['Date'], format='%d%b%Y:%H:%M:%S.%f')
-    
-    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(0.3*lookback, 3.2), sharex=True)
-    fig.patch.set_facecolor('black')
-    for ax in axs:
-        ax.set_facecolor('black')
-        ax.tick_params(axis='x', colors='white')
-        ax.tick_params(axis='y', colors='white')
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%d%b%Y:%H:%M:%S.%f'))
-        ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-        ax.xaxis.set_minor_locator(mdates.HourLocator(interval=6))
-        ax.grid(False)
-        ax.set_xticks([])
-        ax.set_yticks([])
+    rows = sum([1 for df in [equity_df, currency_df, bond_df] if df is not None])
+    fig = make_subplots(rows=rows, cols=1, shared_xaxes=True, vertical_spacing=0.02)
+    fig.update_layout(paper_bgcolor='black', plot_bgcolor='black')
 
-    create_candlestick(axs[0], equity_df)
-    create_candlestick(axs[1], currency_df)
-    create_line(axs[2], bond_df)
+    row = 1
+    if equity_df is not None:
+        equity_df['Date'] = pd.to_datetime(equity_df['Date'], format='%d%b%Y:%H:%M:%S.%f')
+        fig.add_trace(create_candlestick(equity_df), row=row, col=1)
+        row += 1
 
-    plt.tight_layout()
+    if currency_df is not None:
+        currency_df['Date'] = pd.to_datetime(currency_df['Date'], format='%d%b%Y:%H:%M:%S.%f')
+        fig.add_trace(create_candlestick(currency_df), row=row, col=1)
+        row += 1
+
+    if bond_df is not None:
+        bond_df['Date'] = pd.to_datetime(bond_df['Date'], format='%d%b%Y:%H:%M:%S.%f')
+        fig.add_trace(create_line(bond_df), row=row, col=1)
+
+    # Update the layout of the figure
+    fig.update_layout(plot_bgcolor='black', font_color="white", autosize=False, width=lookback*3, height=32)   # noqa
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), paper_bgcolor="black")
+
+    # Disable the legend
+    fig.update_layout(showlegend=False)
+
+    for i in range(1, rows + 1):
+        fig.update_xaxes(row=i, col=1, rangeslider_visible=False)
+        # Update x-axis and y-axis properties for each subplot
+        fig.update_xaxes(row=i, col=1, showgrid=False, zeroline=False, showline=False, gridcolor="white", showticklabels=False, linecolor="white")    # noqa
+        fig.update_yaxes(row=i, col=1, showgrid=False, zeroline=False, showline=False, gridcolor="white", showticklabels=False, linecolor="white")    # noqa
+
+    fig.update_xaxes(type="category")
     return fig
 
-def create_image(equity_df, currency_df, bond_df, width=128, height=128, lookback=5):
+def create_image(equity_df=None, currency_df=None, bond_df=None, width=128, height=128, lookback=5, rgb_channels=1):
     """
     Creates an image from the given dataframes and converts it to a grayscale numpy array.
 
@@ -101,30 +124,33 @@ def create_image(equity_df, currency_df, bond_df, width=128, height=128, lookbac
     Returns:
     np.ndarray: Grayscale numpy array of the image.
     """
-    # Create the matplotlib figure
+    # Create the Plotly figure
     fig = make_figure(equity_df, currency_df, bond_df, lookback=lookback)
 
     # Save the figure to a BytesIO object as a PNG image
     img_bytes = BytesIO()
-    fig.savefig(img_bytes, format="png", bbox_inches='tight', pad_inches=0, dpi=44.3)
-    plt.close(fig)
+    fig.write_image(img_bytes, format="png", width=width, height=height, scale=1)
     img_bytes.seek(0)
 
-    # Open the image using PIL
-    image = Image.open(img_bytes) \
-                    # .resize((width, height))
+    rgb_mode = {
+        1: "L",
+        2: "CMYK",
+        3: "RGB",
+        4: "RGBA"
+    }
 
-    # Set all pixels to white (255) if the pixel value is greater than 20
+    # Open the image using PIL
+    image = Image.open(img_bytes).convert(rgb_mode.get(rgb_channels, "L"))
+
+    # image = image.filter(ImageFilter.SHARPEN)
+
+    # Set all pixels to white (255) if the pixel value is greater than 1
     image = image.point(lambda p: p > 1 and 255)
 
     # Convert the image to a numpy array
     image_array = np.array(image)
 
-    # Custom conversion to grayscale by applying weights to RGB channels
-    grayscale_image_array = np.dot(image_array[..., :3], [0.3, 1, 1])
-    grayscale_image_array = grayscale_image_array.astype(np.uint8)
-
-    return grayscale_image_array
+    return image_array
 
 if __name__ == "__main__":
     # Create sample randomized data
@@ -157,7 +183,9 @@ if __name__ == "__main__":
     })
 
     # Create grayscale image from the dataframes
-    img = create_image(equity_df, currency_df, bond_df)
+    img = create_image(equity_df, currency_df, bond_df,
+                       width=128, height=128, lookback=5,
+                       rgb_channels=3)
     print(img.shape)
 
     # Display and save the image
